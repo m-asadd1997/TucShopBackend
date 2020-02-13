d8f4d9628986011e2242fd30162362a898600e0f
yasir <yasir102900@gmail.com>
2020-02-13 23:56:04 +0500

    dashboard product details

--- src/main/java/com/example/TucShopBackend/Repositories/TransactionsRepository.java
+++ src/main/java/com/example/TucShopBackend/Repositories/TransactionsRepository.java
@@ -2,9 +2,13 @@ package com.example.TucShopBackend.Repositories;
 
 import com.example.TucShopBackend.Models.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface TransactionsRepository extends JpaRepository<Transactions,Long> {
+
+    @Query(value = "select SUM(amount) from transactions ",nativeQuery = true)
+    public double getTransactionsAmount();
 }
