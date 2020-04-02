e5d53c1078d69803d8debe579d6adacedc0bb152
mkbilal92 <mk.bilal92@gmail.com>
2020-04-03 01:49:46 +0500

    ProfitWork

--- src/main/java/com/example/TucShopBackend/Controllers/DashboardController.java
+++ src/main/java/com/example/TucShopBackend/Controllers/DashboardController.java
@@ -1,6 +1,7 @@
 package com.example.TucShopBackend.Controllers;
 
 import com.example.TucShopBackend.Commons.ApiResponse;
+import com.example.TucShopBackend.DTO.ProfitDTO;
 import com.example.TucShopBackend.DTO.SettingsDTO;
 import com.example.TucShopBackend.Models.Product;
 import com.example.TucShopBackend.Models.Settings;
@@ -13,6 +14,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -89,6 +92,12 @@ public class DashboardController {
     public List<Product> getSearchedProducts(@PathVariable String name)
     {
         return dashboardService.getSearchedProducts(name);
+    }
+
+    @GetMapping("/profit/{startDate}/{endDate}")
+    public  ApiResponse getProfit(@PathVariable ("startDate") String startDate, @PathVariable ("endDate") String endDate){
+        return dashboardService.getProfit(startDate, endDate);
+
     }
 
 }
