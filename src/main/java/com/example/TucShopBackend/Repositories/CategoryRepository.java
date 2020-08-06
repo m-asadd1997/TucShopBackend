1b1e2539bfcb6d24a28a27f2ae231c73f72a3dcb
nouman1998 <nouman.ejaz198@gmail.com>
2020-08-06 14:27:31 -0700

    User Crud  and Soft Delete added

--- src/main/java/com/example/TucShopBackend/Repositories/CategoryRepository.java
+++ src/main/java/com/example/TucShopBackend/Repositories/CategoryRepository.java
@@ -2,11 +2,18 @@ package com.example.TucShopBackend.Repositories;
 
 import com.example.TucShopBackend.Models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     public Category findCategoriesByName(String category);
+
+
+    @Query(value ="select * from category where active =1" ,nativeQuery = true)
+    List<Category> getAll();
 
 //    public String findByName(String categoryName);
 
--- src/main/java/com/example/TucShopBackend/Repositories/UserDao.java
+++ src/main/java/com/example/TucShopBackend/Repositories/UserDao.java
@@ -2,10 +2,19 @@ package com.example.TucShopBackend.Repositories;
 
 import com.example.TucShopBackend.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
 
     User findByEmail(String username);
+
+    @Query(value = "select * from tucshop.user u where u.active= 1",nativeQuery = true)
+    List<User> getAllUsers();
+
+
+
 }
