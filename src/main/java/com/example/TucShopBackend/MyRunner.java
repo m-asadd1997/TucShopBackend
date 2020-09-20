76bd6cbf38dc518f5402c90081acf2b41665f71d
mkbilal92 <mk.bilal92@gmail.com>
2020-09-20 07:20:54 +0500

    app_runner

--- src/main/java/com/example/TucShopBackend/MyRunner.java
+++ src/main/java/com/example/TucShopBackend/MyRunner.java
@@ -10,6 +10,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class MyRunner implements CommandLineRunner {
 
@@ -24,7 +26,15 @@ public class MyRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-  userRepository.save(new User("t@t.com ", "test", bcryptEncoder.encode("111"),true, "USER", 1L, "trial"));
+
+        //Uncomment below lines if you want to create User and Admin By My runner.
+
+//        LocalDate accessDate = LocalDate.now();
+//        LocalDate expireDate = LocalDate.now().plusMonths(1);
+//
+//        userRepository.save(new User("t@t.com ", "usertest", bcryptEncoder.encode("111"),true, "USER", 1L, accessDate, expireDate,"trial"));
+//        userRepository.save(new User("a@a.com ", "admintest", bcryptEncoder.encode("111"),true, "ADMIN", 1L, accessDate, expireDate,"trial"));
+
 //        categoryRepository.save(new Category("Chips",null,null));
 
     }
